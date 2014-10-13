ad77f377f5cc0e5a422c9f6618401e96b159231c
micabressan <micabressan@gmail.com>
2014-10-13 20:37:08 -0300

    modificado el admin de inventarios

--- jardinator/inventarios/models.py
+++ jardinator/inventarios/models.py
@@ -3,14 +3,21 @@ from django.db import models
 
 class Unidad(models.Model):
     """ABM unidad."""
-    def __str__(self):
-        return self.nombre
     nombre = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.nombre
 
-class InsumoAccesorio(models.Model):
+    class Meta:
+        verbose_name_plural = "Unidades"
+
+
+class Material(models.Model):
     """ABM insumos y accesorios."""
     descripcion = models.CharField(max_length=250)
     cantidad = models.IntegerField()
     unidad = models.ForeignKey(Unidad, null=False)
     costo = models.FloatField()
+
+    class Meta:
+        verbose_name_plural = "Materiales"
