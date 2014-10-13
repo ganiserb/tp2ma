f5d627f4af8ed10b17d251a273f745473a60595c
pcarrizo <paulii.carrizo@gmail.com>
2014-10-13 20:43:56 -0300

    App Solicitudes

---
+++ jardinator/solicitudes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from contrib.auth import get_user_model
+Usuario = get_user_model()
+
+# Create your models here.
+class Solicitud:
+
+    def __init__(self):
+        pass
+
+    usuario
