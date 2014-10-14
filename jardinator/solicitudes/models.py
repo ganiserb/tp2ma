4bb366c215098bc192b8fe58a911560f3a4f7296
pcarrizo <paulii.carrizo@gmail.com>
2014-10-13 21:43:45 -0300

    Agregado de clave foranea para usuario en modelo Solicitud

--- jardinator/solicitudes/models.py
+++ jardinator/solicitudes/models.py
@@ -4,9 +4,13 @@ from jardinator.settings import AUTH_USER_MODEL
 
 class Solicitud(models.Model):
     """ABM solicitudes."""
-    usuario = AUTH_USER_MODEL
+    usuario = models.ForeignKey(AUTH_USER_MODEL)
     texto = models.CharField(max_length=250)
     fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.usuario.username + " - " + str(self.fecha.strftime(
+            '%x %X'))
 
     class Meta:
         verbose_name_plural = "Solicitudes"
