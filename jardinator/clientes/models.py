babb95b3dd26b8151a4b21fd68a602e5501ba8ab
ganiserb <g.gabriel.b@gmail.com>
2014-10-15 22:57:10 -0300

    finalizado abm de clientes

--- jardinator/clientes/models.py
+++ jardinator/clientes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from jardinator.settings import AUTH_USER_MODEL
 from utilidades.models import Ciudad
+import inventarios
 # from django.contrib.auth import get_user_model
 # User = get_user_model()
 
@@ -11,3 +12,13 @@ class Propiedad(models.Model):
     coordenada_y = models.FloatField()
     direccion = models.CharField(max_length=25)
     ciudad = models.ForeignKey(Ciudad)
+
+
+class Jardin(models.Model):
+    propiedad = models.ForeignKey(Propiedad)
+    nombre = models.CharField(max_length=30)
+    area = models.IntegerField(help_text="Areal del jardín, en metros cuadrados")
+    plantas = models.ManyToManyField(inventarios.models.Planta,
+                                     related_name='jardines')
+    accesorios = models.ManyToManyField(inventarios.models.Material,
+                                        related_name='jardines')
