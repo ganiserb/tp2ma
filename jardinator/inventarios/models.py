0f2a7debf4e19586f468427595a4ae7bd8238583
ganiserb <g.gabriel.b@gmail.com>
2014-11-03 18:19:13 -0300

    arreglada la query de la validación de stock (Sigue sin validar como se debe porque no tiene en cuenta fechas de eventos)

--- jardinator/inventarios/models.py
+++ jardinator/inventarios/models.py
@@ -31,7 +31,7 @@ class Insumo(models.Model):
         if self.stock < 0:
             raise ValidationError('No se admiten valores negativos')
 
-        insumos_asignados = eventos.models.InsumosAsignados.objects.filter(id=self.id)
+        insumos_asignados = eventos.models.InsumosAsignados.objects.filter(insumo__id=self.id)
         if insumos_asignados:
             raise ValidationError('No se puede cambiar el stock de insumos usados en eventos')
         super(Insumo, self).clean()
@@ -84,8 +84,8 @@ class Planta(models.Model):
     def clean(self):
         if self.stock < 0:
             raise ValidationError('No se admiten valores negativos')
-        plantas_asignadas = eventos.models.PlantasAsignadas.objects.filter(id=self.id)
 
+        plantas_asignadas = eventos.models.PlantasAsignadas.objects.filter(planta__id=self.id)
         if plantas_asignadas:
             raise ValidationError('No se puede cambiar el stock de insumos usados en eventos')
 
